d98bbedc63404fb99a97dfb092679c578de61fb0
unknown6656 <unknown84@gmx.de>
2022-02-12 11:31:12 +0100

    updated publisher script

--- publish.bat
+++ publish.bat
@@ -1,2 +1,24 @@
 @echo off
-powershell ./publish.ps1
+    SETLOCAL ENABLEDELAYEDEXPANSION
+    FOR %%p IN (.,pub,publish,Unknown6656.Publisher,../Unknown6656.Publisher) DO (
+        SET _pyfile="%%p/publish.py"
+        ECHO looking for python script inside %%p ...
+        IF EXIST "!_pyfile!" (
+            ECHO invoking python script !_pyfile! ...
+            python "!_pyfile!" "%~f0"
+            GOTO :success
+        )
+    )
+
+:failure
+    ECHO The file 'publish.py' could not be found in one of the following directories:
+    ECHO     - ./
+    ECHO     - pub/
+    ECHO     - publish/
+    ECHO     - Unknown6656.Publisher/
+    ECHO     - ../Unknown6656.Publisher/
+    ECHO Please clone 'https://github.com/Unknown6656-Megacorp/Unknown6656.Publisher' into the current or corresponding directoy.
+
+:success
+    SET "_pyfile="
+    ENDLOCAL
